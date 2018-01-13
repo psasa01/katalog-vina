4e99061e1fe0af73b72b34ff1a5d0841609af893
unknown <psasa@gmail.com.com>
2018-01-13 09:51:28 +0100

    130118

--- handlers/secret.js
+++ handlers/secret.js
@@ -1,9 +1,9 @@
 module.exports = {
     facebook: {
-        clientID: process.env.FACEBOOK_ID || '712105322328284',
-        clientSecret: process.env.FACEBOOK_SECRET || 'd6b34140f75823a680753c615de1eecd',
+        clientID: process.env.FB_ID || '712105322328284',
+        clientSecret: process.env.FB_SECRET || 'd6b34140f75823a680753c615de1eecd',
         profileFields: ['id', 'emails', 'name'],
-        callbackURL: process.env.FACEBOOK_CALLBACK
+        callbackURL: process.env.FB_CALLBACK
     },
     mailer: {
         MAILGUN_USER: 'postmaster@sandbox7510097b9bed48eeb09c15428748bc84.mailgun.org',
